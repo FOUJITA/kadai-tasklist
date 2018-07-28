f6f26f9281aef08e347c9adf3398684ce43e3c8d
norikazu.fujita <efup.maker201@gmail.com>
2018-07-28 13:04:27 +0000

    kadai-tasklist-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,16 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
 
+  
   def index
-    @tasks = Task.order(created_at: :desc).page(params[:page])
+    if logged_in?
+      #@tasks = Task.order(created_at: :desc).page(params[:page])
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(created_at: :desc).page(params[:page])
+      @user = current_user
+    end
   end
 
   def show
@@ -13,7 +21,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -42,6 +51,7 @@ class TasksController < ApplicationController
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+    #redirect_back(fallback_location: root_path)
   end
   
   private
@@ -55,5 +65,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
+  
+  
+  get 'users/index'
+  resources :users, only: [:index, :show, :new, :create]
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
+
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 
   resources :tasks
 end
